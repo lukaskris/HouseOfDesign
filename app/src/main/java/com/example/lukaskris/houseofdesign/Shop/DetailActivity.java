3cd9fbbc869d4bd72b34e455e380325b7f65d803
Lukas <lukaskris12@gmail.com>
2017-07-28 17:18:43 +0700

    add viewpager in actionbar

--- app/src/main/java/com/example/lukaskris/houseofdesign/Shop/DetailActivity.java
+++ app/src/main/java/com/example/lukaskris/houseofdesign/Shop/DetailActivity.java
@@ -1,19 +1,109 @@
 package com.example.lukaskris.houseofdesign.Shop;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.example.lukaskris.houseofdesign.R;
+import com.viewpagerindicator.CirclePageIndicator;
 
 public class DetailActivity extends AppCompatActivity {
+    private ViewPager mViewPager;
+
+    ViewPagerAdapter mViewPagerAdapter;
+    CirclePageIndicator mIndicator;
+
+    int [] mResources = {
+            R.drawable.pemandangan1,
+            R.drawable.pemandangan1,
+            R.drawable.pemandangan1,
+            R.drawable.pemandangan1,
+            R.drawable.pemandangan1,
+            R.drawable.pemandangan1
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
+
+        mViewPager = (ViewPager) findViewById(R.id.detail_viewpager);
+
+        mViewPagerAdapter = new ViewPagerAdapter(this);
+        mViewPager.setAdapter(mViewPagerAdapter);
+        mIndicator = (CirclePageIndicator) findViewById(R.id.detail_circlePageIndicator);
+        mIndicator.setViewPager(mViewPager);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.detail_menu, menu);
+
+        MenuItem item = menu.findItem(R.id.detail_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(item);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    private class ViewPagerAdapter extends PagerAdapter {
+
+        Context mContext;
+        LayoutInflater mLayoutInflater;
+
+        ViewPagerAdapter(Context context) {
+            mContext = context;
+            mLayoutInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        @Override
+        public int getCount() {
+            return mResources.length;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((LinearLayout) object);
+        }
+
+        public Object instantiateItem(ViewGroup container, int position) {
+            View itemView = mLayoutInflater.inflate(R.layout.home_viewpager_row, container, false);
+
+            ImageView imageView = (ImageView) itemView.findViewById(R.id.home_viewpager_imageView);
+            imageView.setImageResource(mResources[position]);
+
+            container.addView(itemView);
+
+            return itemView;
+        }
     }
 }
